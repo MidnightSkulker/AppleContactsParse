f6951ae6d3035de895cf5c4aa0b181375f0732a0
Peter White <desourdesourde@gmail.com>
2018-04-14 11:16:25 -0700

    Code for continuation field

--- src/Main.hs
+++ src/Main.hs
@@ -49,6 +49,13 @@ If some of the code here gets ugly, my excuse is that the input is ugly.
 
 -}
 
+whiteSpace :: GenParser Char st String
+whiteSpace = many (oneOf " \t\n")
+blank :: GenParser Char st Char
+blank = char ' '
+eol :: GenParser Char st Char -- The end of line character is \n
+eol = char '\n'
+
 data Separator = Colon | Semicolon | End deriving (Show)
 toSeparator :: Char -> Separator
 toSeparator ':' = Colon
@@ -60,73 +67,72 @@ itemSeparator = oneOf(":;\n") >>= return . toSeparator
 
 data Attribute = ComplexAttribute { name :: String, value :: String }
                | SimpleAttribute { name :: String } deriving (Show)
+-- Constructor for a Simple Attribute
+mkSimpleAttribute :: String -> Attribute
+mkSimpleAttribute s = SimpleAttribute { name = s }
+
+-- A complex attribute is of the form 'name = value'
 complexAttribute :: GenParser Char st Attribute
 complexAttribute = do { n <- attributeName
                ; _ <- char '='
                ; v <- attributeValue
                ; return ComplexAttribute { name = n, value = v } }
+-- A simple attribute has only a name, no value
 simpleAttribute :: GenParser Char st Attribute
 simpleAttribute = do { n <- attributeValue
                      ; return SimpleAttribute { name = n } }
+-- An attribute name must be non-empty
 attributeName :: GenParser Char st String
 attributeName = many1 (noneOf "=:;\n")
+-- An attribute value can be empty, as in this example: "N:;;;;;"
 attributeValue :: GenParser Char st String
-attributeValue = many1 (noneOf "=:;\n")
+attributeValue = many (noneOf "=:;\n")
 
+-- Parse an attribute
 attribute :: GenParser Char st Attribute
 attribute = try complexAttribute <|> simpleAttribute
 
 data Field = Field { pangalan :: String, attributes :: [Attribute] } deriving (Show)
-field :: GenParser Char st Field
-field = do { a <- attribute
+-- Safely get the last attribute of the field (return Nothing when there are no attributes)
+lastAttribute :: Field -> Maybe Attribute
+lastAttribute f = if null (attributes f) then Nothing else Just (last (attributes f))
+-- Check if a field has no attributs
+noAttributes :: Field -> Bool
+noAttributes = null . attributes
+
+-- Safely add a simple attribute to a field
+addSimpleAttribute :: Field -> String -> Field
+addSimpleAttribute f s = f { attributes = attributes f ++ [mkSimpleAttribute s] }
+
+-- Parse a simple Field
+simpleField :: GenParser Char st Field
+simpleField = do { a <- attributeName
            ; _ <- char ':'
            ; as <- sepBy1 attribute (oneOf ":;")
            ; _ <- eol
-           ; return Field { pangalan = name a, attributes = as } }
+           ; return Field { pangalan = a, attributes = as } }
 
-data Item = Item { text :: String
-                 , separator :: Separator } deriving (Show)
-type Component = [Item]
-componentSeparator :: GenParser Char st Char
-componentSeparator = oneOf ":;\n"
-
-whiteSpace :: GenParser Char st String
-whiteSpace = many (oneOf " \t\n")
-blank :: GenParser Char st Char
-blank = char ' '
--- The end of line character is \n
-eol :: GenParser Char st Char
-eol = char '\n'
-
--- A single item (text followed by separator) of a component
--- Notice at this point no distinction is made between a 'name' and a 'value'
--- The input syntax of VCF is too broken to make that conclusion at this time.
-item :: GenParser Char st Item
-item = do { v <- many (noneOf ":;\n")
-          ; s <- oneOf ":;\n"
-          ; return Item { text = v, separator = toSeparator s } }
-
--- A component is a string of characters other than ':' or ';'
-component :: GenParser Char st Component
-component = many item
-
--- A list of components, separated by ':' or ';'
-components :: GenParser Char st [Component]
-components = sepBy component (oneOf ";:")
-
--- A field is a list of components followed by an end of line
--- field :: GenParser Char st [Component] -- Boo hoo, not a one liner :(
--- field = (continuation >> return []) <|> initial
---   where initial = do { cs <- components
---                      ; char '\n'
---                      ; return cs }
+-- Parse a field
+field :: GenParser Char st Field
+field = do { s <- simpleField
+           ; cs <- continuations
+           ; return (combineContinuationWithField cs s)
+           }
+  where combineContinuationWithField :: [String] -> Field -> Field
+        combineContinuationWithField [] f = f
+        combineContinuationWithField ss f = addSimpleAttribute f (concat ss)
 
 -- Some fields (e.g. PHOTO), have continuation lines for lots of data.
 -- Apparently this are indicated by a leading blank
 -- These continuations always seem to alphanumeric strings
--- TEMP: For now, ignoring the continuation data
-continuation :: GenParser Char st ()
-continuation = blank >> many alphaNum >> eol >> return ()
+continuation :: GenParser Char st String
+continuation = do { blank
+                  ; a <- many alphaNum
+                  ; _ <- eol
+                  ; return a }
+
+continuations :: GenParser Char st [String]
+continuations = many continuation
 
 -- Open and close of an entry
 openEntry, closeEntry :: GenParser Char st ()
@@ -152,14 +158,20 @@ test,t :: GenParser Char () a -> String -> Either ParseError a
 test p testCase = parse p "(unknown)" testCase
 t = test
 
+-- Test attribute parser
 t1,t2,t3 :: Either ParseError Attribute
 t1 = test attribute "a=b"
 t2 = test attribute "ab"
 t3 = test attribute "=b"
-t4 :: Either ParseError Field
+-- Test field parser
+t4,t5,t6,t7,t8,t9,t10 :: Either ParseError Field
 t4 = test field "X-ABUID:4709EC50-7594-4F67-85E1-6870DA65FCBA:ABPerson\n"
 t5 = test field "ab:\n"
 t6 = test field "ab:c\n"
+t7 = test field "N:;;;;\n"
+t8 = test field "PRODID:-//Apple Inc.//Mac OS X 10.13.4//EN\n"
+t9 = test field ":kdkdk\n" -- Shoule fail (left "(unknown)")
+t10 = test field "ORG:Macys;\n"
 
 printConfig = do
   contents <- readFile "stack.yaml"
