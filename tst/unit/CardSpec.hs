e833cbb7b1bc815272ba9e725e0a1f84fa22bf30
Peter White <desourdesourde@gmail.com>
2018-04-19 09:06:51 -0700

    First run of unit test file using stack test

---
+++ tst/unit/CardSpec.hs
@@ -0,0 +1,15 @@
+module CardSpec where
+
+import Test.Hspec
+import Parse
+import Test
+import Gronk
+
+main :: IO ()
+main = error "HIGH THEIR"
+-- main = do { putStrLn "HIGH THEIR"
+--           ; hspec $ do
+--               describe "jsonTest" $ do
+--                 it "X-ABUID:4709EC50-7594-4F67-85E1-6870DA65FCBA:ABPerson\n -> junk"
+--                   jsonTest attribute "X-ABUID:4709EC50-7594-4F67-85E1-6870DA65FCBA:ABPerson\n" `shouldBe` "junk"
+--           }
