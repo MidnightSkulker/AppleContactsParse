c89c8b947721eaa8911b6e105f05a4074319e4dc
Peter White <desourdesourde@gmail.com>
2018-04-20 14:10:01 -0700

    More complex command line arguments compiling

--- src/Args.hs
+++ src/Args.hs
@@ -6,12 +6,28 @@ module Args where
 import Options.Applicative
 import Data.Semigroup ((<>))
 
+-- Parsed arguments: The input (vcf) file and the output (json) file.
+data ParsedArgs = ParsedArgs { input :: String, output :: String } deriving (Show)
+
 commandLineOptions = info (parseArgs <**> helper)
       ( fullDesc
      <> progDesc "Convert Mac Address Book to JSON"
      <> header "Gronk" )
 
-parseArgs :: Parser ()
-parseArgs = () <$
-  switch (long "vcf") <*
-  switch (long "json")
+-- parseArgs :: Parser ()
+-- parseArgs = () <$
+--   switch (long "vcf") <*
+--   switch (long "json")
+
+parseArgs :: Parser ParsedArgs
+parseArgs = ParsedArgs
+      <$> strOption
+          ( long "vcf"
+         <> short 'v'
+         <> metavar "STRING"
+         <> help "input (vcf) file" )
+      <*> strOption
+          ( long "json"
+         <> short 'j'
+         <> metavar "STRING"
+         <> help "output (json) file" )
