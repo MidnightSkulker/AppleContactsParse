6b5a254764d7d15780c5d0f229a9cbd9fbd2d797
Peter White <desourdesourde@gmail.com>
2018-04-23 08:53:25 -0700

    Added filter option to command line

--- src/Args.hs
+++ src/Args.hs
@@ -2,29 +2,31 @@
 -- Parsing the arguments to the program
 module Args where
 
--- import System.Console.ParseArgs
 import Options.Applicative
 import Data.Semigroup ((<>))
-import Data.String
 
 -- Parsed arguments: The input (vcf) file and the output (json) file.
-data ParsedArgs = ParsedArgs { input :: String, output :: String } deriving (Show)
+data ParsedArgs = ParsedArgs { input :: String, output :: String }
+                | FilterArgs deriving (Show)
 
 commandLineOptions = info (parseArgs <**> helper)
-      ( fullDesc
-     <> progDesc "Convert Mac Address Book to JSON"
-     <> header "Gronk" )
+      ( fullDesc <> progDesc "Convert Mac Address Book to JSON" <> header "Gronk" )
 
+-- For Arguments that are explicitly specified
+explicitArgs :: Parser ParsedArgs
+explicitArgs =
+  -- You can use "vcf" and "json" parameters to be more explicit,
+  ParsedArgs <$>
+        strOption ( long "vcf" <> short 'v' <> metavar "STRING" <> help "input .vcf" )
+    <*> strOption ( long "json" <> short 'j' <> metavar "STRING" <> help "output .json" )
+  -- Or you can use positional notation, with the .vcf first and .json second.
+  <|> (ParsedArgs <$> argument str (metavar "FILE") <*> argument str (metavar "FILE"))
+
+-- For Arguments that are left out: The command line is just the name of the program
+-- redirects for stdin and stdout
+filterArgs :: Parser ParsedArgs
+filterArgs = flag' FilterArgs ( long "filter" <> help "Read from stdin" )
+
+-- The complete command line parser
 parseArgs :: Parser ParsedArgs
-parseArgs = ParsedArgs
-      <$> strOption
-          ( long "vcf"
-         <> short 'v'
-         <> metavar "STRING"
-         <> help "input (vcf) file" )
-      <*> strOption
-          ( long "json"
-         <> short 'j'
-         <> metavar "STRING"
-         <> help "output (json) file" )
-      <|> (ParsedArgs <$> argument str (metavar "FILE") <*> argument str (metavar "FILE"))
+parseArgs = explicitArgs <|> filterArgs
