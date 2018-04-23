a153eb0fc9870899630c8cd48985ee4e7b7b4104
Peter White <desourdesourde@gmail.com>
2018-04-23 13:10:58 -0700

    Deleted old code

--- src/Args.hs
+++ src/Args.hs
@@ -7,31 +7,9 @@ import Data.Semigroup ((<>))
 
 -- Parsed Arguments as a list.
 data ListArgs = ListArgs { args :: [String] } deriving (Show)
--- Parsed arguments: The input (vcf) file and the output (json) file.
-data ParsedArgs = ParsedArgs { input :: String, output :: String }
-                | FilterArgs deriving (Show)
 
 commandLineOptions = info (parseArgList <**> helper)
       ( fullDesc <> progDesc "Convert Mac Address Book to JSON" <> header "Gronk" )
-
--- For Arguments that are explicitly specified
-explicitArgs :: Parser ParsedArgs
-explicitArgs =
-  -- You can use "vcf" and "json" parameters to be more explicit,
-  ParsedArgs <$> vcfExplicit <*> jsonExplicit
-  -- Or you can use positional notation, with the .vcf first and .json second.
-  <|> (ParsedArgs <$> vcfPositional <*> jsonPositional)
-  <|> (ParsedArgs <$> vcfExplicit <*> jsonPositional)
-  <|> (ParsedArgs <$> vcfPositional <*> jsonExplicit)
-
--- For Arguments that are left out: The command line is just the name of the program
--- redirects for stdin and stdout
-filterArgs :: Parser ParsedArgs
-filterArgs = flag' FilterArgs ( long "filter" <> help "Read from stdin" )
-
--- The complete command line parser
-parseArgs :: Parser ParsedArgs
-parseArgs = explicitArgs <|> filterArgs
 
 -- The VCF file as an explicit parameter
 vcfExplicit :: Parser String
