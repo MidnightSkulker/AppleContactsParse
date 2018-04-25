071cf8f00df583e2b1b3756189167d431b1af2a8
Peter White <desourdesourde@gmail.com>
2018-04-25 09:30:52 -0700

    Completed argument analysis

--- src/Args.hs
+++ src/Args.hs
@@ -5,6 +5,9 @@ module Args where
 import System.IO
 import Options.Applicative
 import Data.Semigroup ((<>))
+import Data.Either
+import Control.Monad
+import Files
 
 -- Parsed Arguments as a list.
 data Args = Args { args :: [Arg] } deriving (Show)
@@ -17,6 +20,15 @@ argString :: Arg -> String
 argString (VCF s) = s
 argString (JSON s) = s
 argString (Positional s) = s
+isVCF :: Arg -> Bool
+isVCF (VCF s) = True
+isVCF _ = False
+isPositional :: Arg -> Bool
+isPositional (Positional s) = True
+isPositional _ = False
+isJSON :: Arg -> Bool
+isJSON (JSON s) = True
+isJSON _ = False
 
 -- Parse the command line options
 commandLineOptions :: ParserInfo Args
@@ -48,10 +60,52 @@ data Files = Files { input :: Handle, output :: Handle } deriving (Show)
 filterCmd :: Files
 filterCmd = Files { input = stdin, output = stdout }
 
+data ArgError = IOArgError IOError | ArgError String deriving (Show)
 data ArgErrors = ArgErrors { errors :: [String] } deriving (Show)
 
 -- Analyze the parsed arguments
-argAnalysis :: Args -> IO (Either ArgErrors Files)
+argAnalysis :: Args -> IO (Either ArgError Files)
 -- No arguments: It is a filter command
-argAnalysis largs | nArgs largs == 0 = return (Right filterCmd)
-argAnalysis largs | nArgs largs == 1 = undefined
+argAnalysis (Args []) = return (Right filterCmd)
+-- One argument, it is an explicit VCF parameter, or a positional paramter.
+-- Output is assumed to be stdout.
+argAnalysis (Args [a]) | isVCF a || isPositional a =
+  do { eh <- safeOpenFile (argString a) ReadMode
+     ; return (either (Left . IOArgError . id) (\h -> Right (Files {input = h, output = stdout})) eh)
+     }
+-- One argument, it is an explicit JSON parameter. Input is assumed to be stdout.
+argAnalysis (Args [a]) | isJSON a =
+  do eh <- safeOpenFile (argString a) WriteMode
+     return (either (Left . IOArgError . id) (\h -> Right (Files {input = stdin, output = h})) eh)
+-- Two arguments, the first is a VCF or positional (so assumed to be VCF),
+-- the second is JSON or positional (so assumed to be JSON)
+argAnalysis (Args [a,b]) | (isVCF a || isPositional a) && (isJSON b || isPositional b) =
+  do { eah <- safeOpenFile (argString a) ReadMode
+     ; case eah of
+         Left e -> return (Left (IOArgError e))
+         Right ah -> do { ebh <- safeOpenFile (argString b) WriteMode
+                        ; case ebh of
+                            Left e -> return (Left (IOArgError e))
+                            Right bh -> return (Right (Files {input = ah, output = bh}))
+                        }
+     }
+-- Two arguments, the first is a VCF or positional (so assumed to be VCF),
+-- and the second is VCF. This is an error
+argAnalysis (Args [a,b]) | (isVCF a || isPositional a) && isJSON b =
+  return (Left (ArgError "Two VCF Files Specified"))
+-- Two arguments, the first is JSON, and the second is JSON. This is an error
+argAnalysis (Args [a,b]) | isJSON a && isJSON b =
+  return (Left (ArgError "Two JSON Files Specified"))
+-- Two arguments, the first is JSON and the second is VCF or Positional (so assumed to be VCF in this case)
+argAnalysis (Args [a,b]) | isJSON a && (isVCF b || isPositional b) =
+  do { eah <- safeOpenFile (argString a) WriteMode
+     ; case eah of
+         Left e -> return (Left (IOArgError e))
+         Right ah -> do { ebh <- safeOpenFile (argString b) ReadMode
+                        ; case ebh of
+                            Left e -> return (Left (IOArgError e))
+                            Right bh -> return (Right (Files {input = bh, output = ah}))
+                        }
+     }
+-- Three or more arguments is an error
+argAnalysis (Args as) = return (Left (ArgError "More that two parameters specified"))
