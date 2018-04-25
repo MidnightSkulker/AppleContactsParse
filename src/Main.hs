3651e66f7f383cff0a2e44b1faf6e129d4323f0d
Peter White <desourdesourde@gmail.com>
2018-04-25 10:15:13 -0700

    Code to use the parsed files

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,29 @@ module Main where
 
 import Options.Applicative
 import Text.ParserCombinators.Parsec
+import System.IO
+import System.Exit
 import Parse
 import Test
 import Args
 import Files
 
 main :: IO ()
-main = do
-  parsedOptions <- execParser commandLineOptions
-  putStrLn $ "Parsed Options: " ++ show parsedOptions
-  analyzedOptions <- argAnalysis parsedOptions
-  putStrLn $ "Analyzed Options: " ++ show analyzedOptions
---  arubala <- readFile "test/Arubala.test"
---  let json = jsonTest vcf arubala
---  writeFile "Arubala.json" json
+main = do { parsedOptions <- execParser commandLineOptions
+          ; putStrLn $ "Parsed Options: " ++ show parsedOptions
+          ; analyzedOptions <- argAnalysis parsedOptions
+          ; putStrLn $ "Analyzed Options: " ++ show analyzedOptions
+          ; case analyzedOptions of
+              Left argError -> putStrLn (show argError)
+              Right files -> do { putStrLn "Opening files"
+                                ; vcfInput <- hGetContents (input files)
+                                ; putStrLn (show (input files) ++ " is open")
+                                ; let json = jsonTest vcf vcfInput
+                                ; hPutStrLn (output files) json
+                                ; putStrLn (show (output files) ++ " is written")
+                                ; hClose (input files)
+                                ; hClose (output files)
+                                }
+          }
 
 m = main
