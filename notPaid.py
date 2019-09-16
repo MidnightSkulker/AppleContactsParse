a829b6731141907e56a3f8fc7543df84a66e7ad8
Peter White <desourdesourde@gmail.com>
2019-09-16 10:55:16 -0700

    Started python program to access the students.json file

--- notPaid.py
+++ notPaid.py
@@ -2,8 +2,9 @@
 import sys
 import re
 import argparse
+import json
 
-argumentParser = argparse.ArgumentParser(description='Angels Academy Taxes')
+argumentParser = argparse.ArgumentParser(description='Not Paid Students')
 # List of students who have not paid.
 argumentParser.add_argument('--students', help='List of students who have not paid')
 
@@ -13,6 +14,17 @@ nonPayersFile = open(parsedArguments.students)
 # Read the check register of hand written checks
 for student in nonPayersFile:
     print(student.rstrip())
+
+# Read the .json file with the students
+with open("outputs/all.json", "r") as read_file:
+    students = json.load(read_file)
+
+# print(students)
+
+with open("outputs/test.json", "w") as write_file:
+    json.dump(students, write_file, indent=4)
+
+outStudents = json.dumps(students, indent=4)
 
 # Clean up open files
 nonPayersFile.close()
