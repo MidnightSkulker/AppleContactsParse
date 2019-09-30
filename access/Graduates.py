de04c5a8e0e5a11ad13bfba977329f976e6ff2ce
Peter White <desourdesourde@gmail.com>
2019-09-30 14:17:55 -0700

    Progress on Graduates

--- access/Graduates.py
+++ access/Graduates.py
@@ -3,29 +3,38 @@ from datetime import date
 
 class StudentDate(date):
     # Convert string of the form mm/dd/yyyy to a Date
-    def mmddyyyyToDate(d:str):
+    def mmddyyyyToDate(self, d:str):
+        print('mmddyyyy', d)
         d1 = d.split('/')
         if len(d1) == 3:
-            (yyyy, mm, dd) = (d1[2], d1[1], d1[0])
+            (yyyy, mm, dd) = (int(d1[2]), int(d1[1]), int(d1[0]))
             # isoDate = d1[2] + '-' + d1[1] + '-' + d1[0]
             # return date.fromisoformat(isoDate)
-            return datetime.date(d1[2], d1[1], d1[0])
+            return date(yyyy, mm, dd)
         else:
             return None
 
-    def __init__(childDate:str):
-        return mmddyyyyToDate(childDate)
+    def __init__(self, childDate:str):
+        print('__init__', childDate)
+        # return mmddyyyyToDate(childDate)
 
-    def isGraduating(currentYear:str, studentBirthDate:str):
+    def isGraduating(self, currentYear:str, studentBirthDate:str):
         olderThan = mmddyyyyToDate('09/01/' ++ str(currentYear - 5))
         youngerThan = mmddyyyyToDate('09/01/' ++ str(currentYear - 4))
         birthDate = mmddyyyyToDate(studentBirthDat)
         return ((olderThan <= studentBirthDate) and (studentBirthDate <= youngerThan))
 
-def graduates(currentYear:str):
-    print('Graduates!', date.today())
-    olderThan = StudentDate('09/01/2015')
-    youngerThan = StudentDate('09/01/' + str(int(currentYear) - 4))
-    studentBirthDate = '09/12/2015'
-    birthDate = StudentDate('09/12/2015')
-    print('Test1: ', olderThan <=  birthDate)
+def mdyToDate(d:str):
+    d1 = d.split('/')
+    if len(d1) == 3:
+        (yyyy, mm, dd) = (int(d1[2]), int(d1[1]), int(d1[0]))
+        return date(yyyy, mm, dd)
+    else:
+        return None
+
+def graduates(currentYear:str, studentBirthDate: str):
+    cutOffDate = '09/01/'
+    olderThan = mdyToDate(cutOffDate + str(int(currentYear) - 5))
+    youngerThan = mdyToDate(cutOffDate + str(int(currentYear) - 4))
+    birthDate = mdyToDate(studentBirthDate)
+    return ((olderThan <= birthDate) and (birthDate <= youngerThan))
