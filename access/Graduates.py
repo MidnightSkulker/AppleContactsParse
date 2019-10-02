99297efe5af4aeda5daa31e6789299b95324fda4
Peter White <desourdesourde@gmail.com>
2019-10-02 14:50:57 -0700

    Looks like graduates.py is working

--- access/Graduates.py
+++ access/Graduates.py
@@ -1,40 +1,28 @@
 #!/usr/local/bin/python3
 from datetime import date
 
-class StudentDate(date):
-    # Convert string of the form mm/dd/yyyy to a Date
-    def mmddyyyyToDate(self, d:str):
-        print('mmddyyyy', d)
-        d1 = d.split('/')
-        if len(d1) == 3:
-            (yyyy, mm, dd) = (int(d1[2]), int(d1[1]), int(d1[0]))
-            # isoDate = d1[2] + '-' + d1[1] + '-' + d1[0]
-            # return date.fromisoformat(isoDate)
-            return date(yyyy, mm, dd)
-        else:
-            return None
-
-    def __init__(self, childDate:str):
-        print('__init__', childDate)
-        # return mmddyyyyToDate(childDate)
-
-    def isGraduating(self, currentYear:str, studentBirthDate:str):
-        olderThan = mmddyyyyToDate('09/01/' ++ str(currentYear - 5))
-        youngerThan = mmddyyyyToDate('09/01/' ++ str(currentYear - 4))
-        birthDate = mmddyyyyToDate(studentBirthDat)
-        return ((olderThan <= studentBirthDate) and (studentBirthDate <= youngerThan))
-
-def mdyToDate(d:str):
+def mmddyyyyToDate(d:str):
     d1 = d.split('/')
     if len(d1) == 3:
-        (yyyy, mm, dd) = (int(d1[2]), int(d1[1]), int(d1[0]))
+        (yyyy, mm, dd) = (int(d1[2]), int(d1[0]), int(d1[1]))
+        # print ('(yyyy,mm,dd)', (yyyy, mm, dd))
         return date(yyyy, mm, dd)
     else:
         return None
 
-def graduates(currentYear:str, studentBirthDate: str):
-    cutOffDate = '09/01/'
-    youngerThan = mdyToDate(cutOffDate + str(int(currentYear) - 5))
-    olderThan = mdyToDate(cutOffDate + str(int(currentYear) - 6))
-    birthDate = mdyToDate(studentBirthDate)
+def isGraduating(currentYear:str, studentBirthDate:str):
+    olderThan = mmddyyyyToDate('09/01/' + str(int(currentYear) - 5))
+    youngerThan = mmddyyyyToDate('09/01/' + str(int(currentYear) - 4))
+    birthDate = mmddyyyyToDate(studentBirthDate)
     return ((olderThan <= birthDate) and (birthDate <= youngerThan))
+
+# class DateExtended(date):
+#     def __new__(self, childDate:str):
+#         # FOR THIS CLASS ONLY
+#         # because __new__ for the date class creates the instance you need
+#         # to pass the arguments to the superclass here
+#         # and **not** in the __init__
+#         return super().__new__(mmddyyyyToDate(childDate))
+#     def __init__(self, year: int, month: int, day: int, date_format: str=None):
+#         # datetime.date.__init__ is just object.__init__ so it takes no arguments.
+#         super().__init__()  
